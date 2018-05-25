063684c83d4a786db69e962c43ba0669f3fb30c1
phulinMN <pongpaiMN@gmail.com>
2018-05-25 18:10:25 +0700

    message

--- server/api/message.js
+++ server/api/message.js
@@ -3,8 +3,9 @@ import { Router } from 'express'
 const router = Router()
 
 router.post('/message', function (req, res, next) {
+    // console.log();
     // res.json()
-    res.json(req);
+    res.json(req.body);
 })
 
 export default router
